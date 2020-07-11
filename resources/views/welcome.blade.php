462014ffd63db2a9f4ab7e3862df0463ad56af84
D.B.F <saluei@yahoo.com>
2020-07-11 00:31:50 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test Change version 3
+                    server change
                 </div>
 
                 <div class="links">
