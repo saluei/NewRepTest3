6bb5da0b839b2347cce8797c00d731a3d41060b1
D.B.F <saluei@yahoo.com>
2020-07-11 00:26:29 -0700

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test3
+                    Test Change version 3
                 </div>
 
                 <div class="links">
